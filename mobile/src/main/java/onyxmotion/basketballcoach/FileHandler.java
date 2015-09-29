dac88ddcf11f77d68bd5ff48d1b5ce99b6969696
leadmarissa <emailmarissawu@gmail.com>
2015-09-29 17:05:54 -0400

    Changed layout to green, added support for round and square, changed text of button

---
+++ mobile/src/main/java/onyxmotion/basketballcoach/FileHandler.java
@@ -0,0 +1,83 @@
+package onyxmotion.basketballcoach;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+
+/**
+ * Handles writing to file
+ * Created by Vivek on 2014-07-02.
+ */
+public class FileHandler {
+
+    private static final String TAG = "FileHandler", f = Const.f, t = Const.t;
+
+    private static FileHandler[] fileHandler = {null, null};
+
+    private PrintWriter writer;
+
+    private String file, device;
+
+    private boolean isInitialized = false;
+
+    public static FileHandler getHandler(String d) {
+        int i = d.equals(Const.MOBILE) ? 0 : 1;
+        if (fileHandler[i] == null) fileHandler[i] = new FileHandler(d);
+        return fileHandler[i];
+    }
+
+    public static String filenameDefault(String device) {
+        return "OnyxData" + device + new SimpleDateFormat("yyMMddHHmmss")
+            .format(System.currentTimeMillis()) + ".txt";
+    }
+
+    private FileHandler(String d) { device = d; }
+
+    public void initialize(String filename) {
+	    if (isInitialized) return;
+        file = filename;
+	    Log.d(TAG, "Initializing: " + file);
+        try {
+            writer = new PrintWriter(
+                Environment.getExternalStorageDirectory().getAbsolutePath()
+                + f + filename, "UTF-8");
+        } catch (Exception e) {
+            Log.e(TAG, "Error initializing file " + file);
+        }
+        isInitialized = true;
+    }
+
+    public void close() {
+        if (!isInitialized) return;
+        writer.flush();
+        writer.close();
+        if (writer.checkError())
+            Log.e(TAG, "Error in closing writer " + file);
+        isInitialized = false;
+    }
+
+    public void printData(RecordData rec) {
+        float[] aData = rec.acc(), rData = rec.rot(), gData = rec.gyr();
+        println("#N-=" + rec.device() + t + rec.index() + t + rec.time());
+        println("#A-=" + aData[0] + t + aData[1] + t + aData[2]);
+        println("#R-=" + rData[0] + t + rData[1] + t + rData[2]
+            + t + rData[3] + t + rData[4]);
+        println("#G-=" + gData[0] + t + gData[1] + t + gData[2]);
+	    Log.d(TAG, "Printing record " + rec.device() + t + rec.index());
+    }
+
+    public void print(String text) {
+        Log.d(TAG,"print: " + text);
+        if (isInitialized)
+            writer.print(text);
+    }
+
+    public void println(String text) {
+        if (!isInitialized) return;
+    //    Log.d(TAG,"println: " + text);
+        writer.println(text);
+
+    }
+}
---
+++ mobile/src/main/java/onyxmotion/basketballcoach/MainActivity.java
@@ -0,0 +1,119 @@
+package onyxmotion.basketballcoach;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    String TAG = "MainActivity";
+	TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		textView = (TextView) findViewById(R.id.main_text);
+        initializeHandlers();
+
+
+        Log.d(TAG, "MainActivity initialized");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SensorHandler.getHandler().register();
+	    DataHandler.getHandler().connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SensorHandler.getHandler().unregister();
+	    DataHandler.getHandler().disconnect();
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return item.getItemId() == R.id.action_settings
+            || super.onOptionsItemSelected(item);
+    }
+
+    private void initializeHandlers() {
+        DataHandler.getHandler().initialize(this,
+            new RecordData.RecordCallable() {
+                @Override
+                public void call(RecordData record) {
+                    onData(record);
+                }
+            }
+        );
+        SensorHandler.getHandler().initialize(this,
+            new RecordData.RecordCallable() {
+                @Override
+                public void call(RecordData record) {
+                    onSensor(record);
+                }
+            }
+        );
+
+        FileHandler.getHandler(Const.MOBILE)
+            .initialize(FileHandler.filenameDefault(Const.MOBILE));
+        FileHandler.getHandler(Const.WEAR)
+            .initialize(FileHandler.filenameDefault(Const.WEAR));
+
+    }
+
+    public void onSensor(RecordData record) {
+
+    }
+
+    public void onData(RecordData rec) {
+	    String t = Const.t, n = Const.n;
+	    float[] aData = rec.acc(), rData = rec.rot(), gData = rec.gyr();
+	    textView.setText("#N-=" + rec.device() + t + rec.index() + t + rec.time()
+	     + n + "#A-=" + aData[0] + t + aData[1] + t + aData[2]
+	     + n + "#R-=" + rData[0] + t + rData[1] + t + rData[2] + t + rData[3] + t + rData[4]
+	     + n + "#G-=" + gData[0] + t + gData[1] + t + gData[2]);
+/*        DataMap dm = DataHandler.getHandler().currDataMap;
+        if (dm == null) return;
+        float aData[] = dm.getFloatArray(DataHandler.ACC),
+              gData[] = dm.getFloatArray(DataHandler.GYR);
+        String text = "Mobile Data" + t + dm.getInt(DataHandler.NUM) + t +
+            SimpleDateFormat.getDateTimeInstance()
+            .format(new Date(dm.getLong(DataHandler.TSN) / 1000000L)) + n
+            + "Accel:" + t + aData[0] + t + aData[1] + t + aData[2] + n
+            + "Gyros:" + t + gData[0] + t + gData[1] + t + gData[2] + n;
+*/
+//        mTextView.setText(DataHandler.getHandler().text);
+    }
+
+    public void onClick(View button) {
+        ((Button) button).setText("Smart Tips "
+            + (SensorHandler.getHandler().toggleRecord() ? "ON" : "OFF"));
+    }
+
+	public void startWatch(View button) {
+		DataHandler.getHandler().sendStartWatch();
+	}
+}
